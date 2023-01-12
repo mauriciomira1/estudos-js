fec2f9ac6600e8db129ef3c672e369778fb7a66f
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-01-12 14:05:55 -0300

    mais aulas

---
+++ Curso em vídeo/aula12/ex013.js
@@ -0,0 +1,7 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch (diaSem) {
+    case 0
+    console.log
+}
---
+++ Curso em vídeo/ex017/script.js
@@ -0,0 +1,44 @@
+function calcular() {
+    var data = new Date()
+    var agora = data.getFullYear()
+    var year = document.querySelector('input#ano')
+    var rsex = document.getElementsByName('sexo')
+    var result = document.getElementById('txt')
+    var idade = agora - Number(year.value)
+    var genero = ''
+    var pict = document.createElement('img')
+    pict.setAttribute('id','foto')
+
+    if (year.value >= agora || year.value.length == 0 || year.value == 0) {
+        window.alert('[ERRO!] Informe um valor válido')
+    } else {
+    
+        if (rsex[0].checked) {
+            genero = 'Homem'
+            
+            if (idade < 20) {
+                pict.setAttribute('src','img/homem_jovem.png')
+            } else if (idade >= 20 && idade < 50) {
+                pict.setAttribute('src','img/homem_adulto.png')
+            } else 
+                pict.setAttribute('src','img/homem_idoso.png')
+        } else if (rsex[1].checked ) {
+        genero = 'Mulher'
+        
+        if (idade < 20) {
+            pict.setAttribute('src','img/mulher_jovem.png')
+        } else if (idade >= 20 && idade < 50) {
+            pict.setAttribute('src','img/mulher_adulta.png')
+        } else 
+        pict.setAttribute('src','img/mulher_idosa.png')
+        }
+        result.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        result.appendChild(pict)
+    }
+
+
+/*     var sexo = document.querySelector
+    var text = document.querySelector('div#txt')
+    var imgem = document.querySelector('img#foto')
+ */
+}
