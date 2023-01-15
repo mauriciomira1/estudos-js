cbb5d1ad1a94a18f874b991a3fc84957ea1b6fad
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-01-15 11:26:32 -0300

    mais estudos

---
+++ Curso em vídeo/aula17/objeto01.js
@@ -0,0 +1,6 @@
+let amigo = {nome: 'Maurício', 
+sexo:'M', 
+engordar(p){
+
+}}
+console.log(amigo.nome)  
---
+++ Curso em vídeo/desafio_calculadora/script.js
@@ -0,0 +1,9 @@
+let num = []
+
+for (let pos in num ) {
+    if ()
+}
+
+let num1 = document.querySelector('input#n1')
+let num1 = document.querySelector('input#n1')
+
